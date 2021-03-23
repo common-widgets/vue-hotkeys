0537f0f32c4531a3d8138661390ad7b90c6ad5dc
马成达 <machengda@seer-robotics.com>
2021-03-23 20:04:58 +0800

    update: register

--- src/libs/hotkeys.ts
+++ src/libs/hotkeys.ts
@@ -1,12 +1,17 @@
 import hotkeys, { HotkeysEvent } from "hotkeys-js";
 
+let hkCache = false
 export default {
   install(app: any) {
     app.directive("hotkey", (el: Element, binding: any) => {
+      console.log("binding", binding)
       const stop = binding?.modifiers?.stop;
       const prevent = binding?.modifiers?.prevent;
       const configs = binding?.value;
       if (configs) {
+        // 清理缓存
+        if (hkCache) hotkeys.unbind()
+        hkCache = true
         Object.keys(configs).forEach((key: string) => {
           hotkeys(key, function(e: KeyboardEvent, handler: HotkeysEvent) {
             stop && e.stopPropagation();
